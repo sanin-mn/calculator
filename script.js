60a9b7235ef55223efa884e0e7d7cf3753d535da
Muhammed Sanin <93256890+Sani369@users.noreply.github.com>
2022-12-04 00:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonclick(val){
+    document.getElementById("screen").value+=val
+}
+function valueclear(){
+    document.getElementById("screen").value=""
+}
+function equalclick() {
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
